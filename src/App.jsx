a49f62405158f62897f220e3849d0f60b82eaef0
Abhishek Dasgupta <abhishek20dgp@gmail.com>
2024-01-06 01:11:39 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import {Search} from './Components'
 import {Loader} from './Components'
 import {Navbar} from './Components'
+import {About} from './Components'
 import './App.css'
 
 function App() {
@@ -22,7 +23,7 @@ function App() {
       <Loader
       isLoading={isLoading}
       />
-      <div className="flex flex-col bg-gray-100 border-dotted border-2 border-gray-300 rounded-lg p-4 mt-10 items-center w-full h-56 overflow-auto">
+      <div className="flex flex-col bg-gray-100 border-2 border-blue-500 rounded-lg p-4 mt-10 items-center w-full h-56 overflow-auto">
       <p className="text-lg text-gray-700">{answer}</p>
       </div>
       </div>
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,49 +1,47 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './About.css'; 
 
-function About({ onClose }) {
-  return (
-    
-<div>
-<button data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-  Toggle modal
-</button>
-
-<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-2xl max-h-full">
-      
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                    Terms of Service
-                </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
+export default function Modal() {
+    const [modal, setModal] = useState(false);
+  
+    const toggleModal = () => {
+      setModal(!modal);
+    };
+  
+    if(modal) {
+      document.body.classList.add('active-modal')
+    } else {
+      document.body.classList.remove('active-modal')
+    }
+  
+    return (
+      <>
+        <button onClick={toggleModal} className="btn-modal">
+          About
+        </button>
+  
+        {modal && (
+          <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal-content">
+              <div className='text-2xl mb-6'>About BrainSearch</div>
+              <p>
+              Welcome to BrainSearch – your AI-powered news and insights hub! Experience precision with our AI algorithms, delivering real-time updates and curated content tailored just for you. 
+              <br/>
+              <br/>
+              Say goodbye to information overload and hello to a user-friendly interface, customizable alerts, and the future of informed decision-making. Dive into the latest news effortlessly with BrainSearch.
+              <br/>
+              <br/>
+               Intelligence meets exploration here!
+              
+              </p>
+              <button className="close-modal" onClick={toggleModal}>
+                Close
+              </button>
             </div>
-           
-            <div class="p-4 md:p-5 space-y-4">
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                </p>
-                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                </p>
-            </div>
-          
-            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                <button data-modal-hide="default-modal" type="button" class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-
-  )
-}
-
-export default About
+          </div>
+        )}
+        
+      </>
+    );
+  }
